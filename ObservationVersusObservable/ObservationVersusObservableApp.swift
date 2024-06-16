1037441310fabc35c6c0c8fab32175c80f2084e8
Didier <did35@yahoo.com>
2024-06-16 12:32:45 +0200

    Initial Commit

---
+++ ObservationVersusObservable/ObservationVersusObservableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationVersusObservableApp.swift
+//  ObservationVersusObservable
+//
+//  Created by Didier Delhaisse on 16/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ObservationVersusObservableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
